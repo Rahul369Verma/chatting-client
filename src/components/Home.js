db3b19715ad9682332f34e197c6d3539c55e9aec
Rahul Verma <raj369verma@gmail.com>
2022-01-17 00:19:04 +0530

    remove home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import {useContext} from "react"
+import { useContext } from "react"
 import Auth from '../isAuth';
 import { Context } from '../context';
 import Loader from "./loader/loader";
@@ -7,19 +7,19 @@ import Loader from "./loader/loader";
 
 const Home = () => {
 
-	const { state } = useContext(Context)
+  const { state } = useContext(Context)
 
-
+  window.location.replace("/chatting")
   if (Auth.getAuth() || state.email === false) {
     return (
-      <div className="whole">
-        <h1>Thanks for checkout</h1>
-        <br />
-        <h3>Do Register or login to see Chatting App</h3>
-      </div>
-    )
-  } else if (state.email === null) {
-    return (
+      //     <div className="whole">
+      //       <h1>Thanks for checkout</h1>
+      //       <br />
+      //       <h3>Do Register or login to see Chatting App</h3>
+      //     </div>
+      //   )
+      // } else if (state.email === null) {
+      // return (
       <div>
         <Loader />
       </div>
